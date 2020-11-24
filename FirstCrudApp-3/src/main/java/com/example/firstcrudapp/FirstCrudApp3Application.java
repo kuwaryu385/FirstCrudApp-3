1019cf41fcd21c9c30b39bda1083ec75022d0eda
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-11-24 13:41:06 +0900

    Firstcommit 初期値

---
+++ FirstCrudApp-3/src/main/java/com/example/firstcrudapp/FirstCrudApp3Application.java
@@ -0,0 +1,13 @@
+package com.example.firstcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCrudApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCrudApp3Application.class, args);
+	}
+
+}
---
+++ FirstCrudApp-3/src/test/java/com/example/firstcrudapp/FirstCrudApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCrudApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

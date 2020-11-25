4fbb21e82a782c0d3ae7b07b471cdabc23d3dcd6
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-11-25 13:17:03 +0900

    modelの作成　getter setter lombokでじっそうしてみた

---
+++ FirstCrudApp-3/src/main/java/com/example/firstcrudapp/domein/MyBooksModel.java
@@ -0,0 +1,17 @@
+package com.example.firstcrudapp.domein;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class MyBooksModel {
+
+	private Integer id;
+
+	private String title;
+
+	private String author;
+
+}

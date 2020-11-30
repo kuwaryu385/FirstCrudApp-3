006a5765cc4b4e153251e3b4f286b334c3fb8895
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-11-30 12:43:11 +0900

    詳細画面の実装　修正中

--- FirstCrudApp-3/src/main/java/com/example/firstcrudapp/controller/MyBooksController.java
+++ FirstCrudApp-3/src/main/java/com/example/firstcrudapp/controller/MyBooksController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,19 +21,26 @@ public class MyBooksController {
 	@Autowired
 	private MyBooksService myBooksService;
 
-	//テーブル一覧画面の表示
+	//テーブル一覧画面の表示　ReadAll処理
 	@GetMapping
 	public String index(Model model) {
 		model.addAttribute("myBooks", myBooksService.selectAll());
 		return "index";
 	}
 
+	//本のIDにて、本の内容を取得
+	@GetMapping("{id}")
+	public String show(@PathVariable Integer id, Model model) {
+		model.addAttribute(" aBook", myBooksService.selectOne(id));
+		return "show";
+	}
+
 	//新規作成画面の表示
 
 	@GetMapping("new")
-	public String getBooksNew(Model model) {
-		MyBooksModel mybooks = new MyBooksModel();
-		model.addAttribute("myBook", mybooks);
+	public String getBooksNew(@ModelAttribute("myBook") MyBooksModel myBooksModel, Model model) {
+		//		MyBooksModel mybooks = new MyBooksModel();
+		//		model.addAttribute("myBook", mybooks);
 		return "new";
 	}
 
